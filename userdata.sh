c44fc9045e23033c35af84f47a71d104ab55f2c9
venkykolli <157064180+venkykolli@users.noreply.github.com>
2025-02-20 23:17:24 +0530

    Create userdata.sh

---
+++ userdata.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Print a message
+echo "Hello, World!"
+
+# Display system information
+echo "Current User: $(whoami)"
+echo "Current Date and Time: $(date)"
+echo "Uptime: $(uptime -p)"
+echo "Current Directory: $(pwd)"
